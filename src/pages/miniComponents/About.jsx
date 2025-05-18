bcbc9e114d227381213e73d0faaef1041fe524c2
VijaySinghSaaud <saudvijaysingh@gmail.com>
2025-05-18 22:42:21 +0530

    portfolio img

--- src/pages/miniComponents/About.jsx
+++ src/pages/miniComponents/About.jsx
@@ -26,7 +26,7 @@ const About = () => {
         <div className="grid md:grid-cols-2 my-8 sm:my-20 gap-14 px-4 sm:px-8">
           <div className="flex justify-center items-center">
             <img
-              src="/img.JPG.png"
+              src="/img.JPG"
               alt="Vijay Singh Saud"
               className=" p-2 sm:p-4  h-[240px] sm:h-[340px] md:h-[350px] lg:h-[450px] shadow-lg"
             />
